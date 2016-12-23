19f42d974015d84eae98e9cc3107441594622891
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2016-12-23 12:38:04 +0000

    fix paths in sed

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # see this issue: https://github.com/sciurius/chordpro/issues/1
 # here I use open access DejaVuSans.ttf
 chordpro --print-default-config > mycfg.json
-sed -i -- 's/"name" : "Times-Roman"/'"file" : "fonts/open-sans/OpenSans-Regular.ttf"'/g' mycfg.json
-sed -i -- 's/"name" : "Times-Bold"/'"file" : "fonts/open-sans/OpenSans-Bold.ttf"'/g' mycfg.json
+sed -i -- 's+"name" : "Times-Roman"+"file" : "fonts/open-sans/OpenSans-Regular.ttf"+g' mycfg.json
+sed -i -- 's+"name" : "Times-Bold"+"file" : "fonts/open-sans/OpenSans-Bold.ttf"+g' mycfg.json
 
 mkdir pdf
 
