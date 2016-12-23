e02169f0ccafa59473c970327df65ca80ed02c1d
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2016-12-23 13:17:29 +0000

    and again

--- chordpro.sh
+++ chordpro.sh
@@ -1,3 +1,3 @@
-chordpro --cfg mycfg.json -t 8 -c 8 -n -p 1 -o pdf/all_songs.pdf \
+chordpro --cfg mycfg.json -t 8 -c 8 -p 1 -i -o pdf/all_songs.pdf \
     cho/ddt-osen.cho \
     cho/kino-peremen.cho
