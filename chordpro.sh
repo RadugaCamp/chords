6e5d12b35b5ce69cebc2ca3e7951dc42a321e3be
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2016-12-23 13:28:21 +0000

    don't know how to add page numbers...

--- chordpro.sh
+++ chordpro.sh
@@ -1,5 +1,3 @@
-cat cho/ddt-osen.cho \
-    cho/kino-peremen.cho \
-    > cho/all_songs.cho
-
-chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf cho/all_songs.cho
+chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf \
+    cho/ddt-osen.cho \
+    cho/kino-peremen.cho
