4d9722bd43797e7f5377d33d0465eac37598ebf0
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2016-12-23 13:25:04 +0000

    merge file before processing

--- chordpro.sh
+++ chordpro.sh
@@ -1,3 +1,5 @@
-chordpro --cfg mycfg.json -t 8 -c 8 -p 1 -i -o pdf/all_songs.pdf \
-    cho/ddt-osen.cho \
-    cho/kino-peremen.cho
+cat cho/ddt-osen.cho \
+    cho/kino-peremen.cho \
+    > cho/all_songs.cho
+
+chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf cho/all_songs.cho
