07ec65e0c2c4a9ceba42ce9b7dd8f659c5d643b8
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-01-23 22:57:46 +0000

    two pages per page

--- chordpro.sh
+++ chordpro.sh
@@ -1,4 +1,4 @@
-chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf \
+chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf -2 \
     cho/ddt-osen.cho \
     cho/kino-peremen.cho \
     cho/robbie-williams-i-love-my-life.cho
