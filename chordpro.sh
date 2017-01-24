d1bc0bc29019ee6a1cb1d4358b4fce3a57ee8c4a
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-01-24 21:34:49 +0000

    try column break

--- chordpro.sh
+++ chordpro.sh
@@ -1,4 +1,4 @@
-chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf -2 \
+chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf \
     cho/ddt-osen.cho \
     cho/kino-peremen.cho \
     cho/robbie-williams-i-love-my-life.cho \
