82f7cb8cd7ce7b3c4fcf4e4b7093f351caab2da2
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-01-24 21:16:04 +0000

    add a song

--- chordpro.sh
+++ chordpro.sh
@@ -1,4 +1,5 @@
 chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf -2 \
     cho/ddt-osen.cho \
     cho/kino-peremen.cho \
-    cho/robbie-williams-i-love-my-life.cho
+    cho/robbie-williams-i-love-my-life.cho \
+    cho/robbie-williams-angels.cho
