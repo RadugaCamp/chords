c0573044558bb4d42322677a887fe5ced1998d9f
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-08-29 14:00:43 +0100

    fix name

--- chordpro.sh
+++ chordpro.sh
@@ -3,6 +3,6 @@ chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf \
     cho/kino-peremen.cho \
     cho/kino-gruppa-krovi.cho \
     cho/nautilus-pompilius-progulki-po-vode.cho \
-    cho/nautilus-pompilius-progulki-dykhanie.cho \
+    cho/nautilus-pompilius-dykhanie.cho \
     cho/robbie-williams-i-love-my-life.cho \
     cho/robbie-williams-angels.cho
