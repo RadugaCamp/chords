8ba788e0a4c6e7dd164b2853c8a57800e35b79d0
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-09-02 09:11:55 +0100

    add song

--- chordpro.sh
+++ chordpro.sh
@@ -4,5 +4,6 @@ chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf \
     cho/kino-gruppa-krovi.cho \
     cho/nautilus-pompilius-progulki-po-vode.cho \
     cho/nautilus-pompilius-dykhanie.cho \
+    cho/leonid-agutin-na-sirenevoy-lune.cho \
     cho/robbie-williams-i-love-my-life.cho \
     cho/robbie-williams-angels.cho
