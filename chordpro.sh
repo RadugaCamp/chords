e37ffe11fc039c37315b4f46793ad27a0da3be45
wikiselev <vladimir.yu.kiselev@gmail.com>
2017-09-26 23:15:56 +0100

    try build all

--- chordpro.sh
+++ chordpro.sh
@@ -1,9 +1,1 @@
-chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf \
-    cho/ddt-osen.cho \
-    cho/kino-peremen.cho \
-    cho/kino-gruppa-krovi.cho \
-    cho/nautilus-pompilius-progulki-po-vode.cho \
-    cho/nautilus-pompilius-dykhanie.cho \
-    cho/leonid-agutin-na-sirenevoy-lune.cho \
-    cho/robbie-williams-i-love-my-life.cho \
-    cho/robbie-williams-angels.cho
+chordpro --cfg mycfg.json -t 8 -c 8 -i -o pdf/all_songs.pdf cho/*
